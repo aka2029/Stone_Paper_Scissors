97d952c49914a83b2e0a500dc0efe4225bedc996
aka2029 <asaks22@gmail.com>
2020-05-01 23:55:49 +0530

    new commit

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
@@ -14,23 +14,35 @@ function getComputerChoice() {
   return choices[randomNumber];
 }
 
+function win() {
+  userScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+}
+function lose() {
+  console.log("lose");
+}
+function draw() {
+  console.log("draw");
+}
+
 function game(userChoice) {
   const computerChoice = getComputerChoice();
   switch (userChoice + computerChoice) {
-    case "rp":
+    case "rs":
     case "pr":
     case "sp":
-      console.log("USER WINS.");
+      win();
       break;
     case "rp":
     case "ps":
-    case "rs":
-      console.log("USER LOSES.");
+    case "sr":
+      lose();
       break;
     case "rr":
     case "pp":
     case "ss":
-      console.log("It's a draw.");
+      draw();
       break;
   }
 }
